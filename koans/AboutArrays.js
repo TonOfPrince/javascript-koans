89e8c25b818e91eb6cf44f245975014d289da6a9
Brett Leibowitz <brettleibo@gmail.com>
2014-09-17 01:21:51 -0500

    Array Koan mass change

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -48,7 +48,7 @@ describe("About Arrays", function() {
     
     expect(array.slice(0, 1)).toEqual(['peanut']);
     expect(array.slice(0, 2)).toEqual(['peanut', 'butter']);
-    expect(array.slice(2, 2)).toEqual([]);
+    expect(array.slice(2, 2)).toEqual(['and','butter','peanut']);
     expect(array.slice(2, 20)).toEqual(['and', 'jelly']);
     expect(array.slice(3, 0)).toEqual(['and', 'butter', 'peanut']);
     expect(array.slice(3, 100)).toEqual(['jelly']);
